3cd1ada87aeeeae766ad992b1ebbf312e1ce1507
Michael Luu <67599521+kemonprogrammer@users.noreply.github.com>
2021-12-04 22:51:09 +0100

    Refactor 2021/Day2: Change values from ArrayList to int-Array
    Because the amount of values (radius, depth (, aim)) don't change, it's
    better to use an int-Array

--- src/aoc2021/day2/Day2.java
+++ src/aoc2021/day2/Day2.java
@@ -2,7 +2,6 @@ package aoc2021.day2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 // Enum
@@ -15,22 +14,21 @@ public class Day2 {
 
         // Task one
         int radius = 0, depth = 0, product;
-        ArrayList<Integer> values = new ArrayList<>();
-        values.add(radius);
-        values.add(depth);
+        int[] values = new int[2];
+        values[0] = radius;
+        values[1] = depth;
 
         // Task two
         int aim = 0;
-        ArrayList<Integer> valuesTwo = new ArrayList<>();
-        valuesTwo.add(radius);
-        valuesTwo.add(depth);
-        valuesTwo.add(aim);
+        int[] valuesTwo = new int[3];
+        valuesTwo[0] = radius;
+        valuesTwo[1] = depth;
+        valuesTwo[2] = aim;
 
         try {
             // Setup reader
             String pathname = "src/aoc2021/day2/day2.txt";
-            File input = new File(pathname);
-            Scanner reader = new Scanner(input);
+            Scanner reader = new Scanner(new File(pathname));
             StringBuilder line = new StringBuilder();
 
             // Read the file
@@ -47,62 +45,62 @@ public class Day2 {
                 taskOne(values, direction, value);
                 taskTwo(valuesTwo, direction, value);
             }
-        } catch (FileNotFoundException FNFE) {
-            System.out.println(FNFE.getMessage());
+            reader.close();
+        } catch (FileNotFoundException fnfe) {
+            System.out.println(fnfe.getMessage());
         }
 
         // calculate product of task one
-        radius = values.get(0);
-        depth = values.get(1);
+        radius = values[0];
+        depth = values[1];
         product = radius * depth;
         int taskNumber = 1;
-        String format = "Task %d: horizontal position(%d)*Depth(%d)=product(%d)\n";
+        String format = "Task %d: %-7d(Horizontal position) * %-5d(Depth) = %-11d(product)\n";
         System.out.printf(format, taskNumber, depth, radius, product);
 
         // calculate product of task two
-        radius = valuesTwo.get(0);
-        depth = valuesTwo.get(1);
+        radius = valuesTwo[0];
+        depth = valuesTwo[1];
         product = radius * depth;
         taskNumber = 2;
         System.out.printf(format, taskNumber, depth, radius, product);
     }
 
 
-    public static void taskOne(ArrayList<Integer> values, String direction, int value) {
+    public static void taskOne(int[] values, String direction, int value) {
         // Change depth or radius based on direction
         if (direction.equals(forward.toString())) {
             // forward
-            int newRadius = values.get(0) + value;
-            values.set(0, newRadius);
+            int newRadius = values[0] + value;
+            values[0] = newRadius;
         } else if (direction.equals(up.toString())) {
             // up
-            int newDepth = values.get(1) - value;
-            values.set(1, newDepth);
+            int newDepth = values[1] - value;
+            values[1] = newDepth;
         } else if (direction.equals(down.toString())) {
             // down
-            int newDepth = values.get(1) + value;
-            values.set(1, newDepth);
+            int newDepth = values[1] + value;
+            values[1] = newDepth;
         }
-
     }
 
-    public static void taskTwo(ArrayList<Integer> values, String direction, int value) {
+    public static void taskTwo(int[] values, String direction, int value) {
 
         // Change depth or radius based on direction
         if (direction.equals(forward.toString())) {
             // forward
-            int newRadius = values.get(0) + value;
-            int newDepth = values.get(1) + values.get(2) * value;
-            values.set(0, newRadius);
-            values.set(1, newDepth);
+            int newRadius = values[0] + value;
+            int newDepth = values[1] + values[2] * value;
+            values[0] = newRadius;
+            values[1] = newDepth;
         } else if (direction.equals(up.toString())) {
             // up
-            int newAim = values.get(2) - value;
-            values.set(2, newAim);
+            int newAim = values[2] - value;
+            values[2] = newAim;
         } else if (direction.equals(down.toString())) {
             // down
-            int newAim = values.get(2) + value;
-            values.set(2, newAim);
+            int newAim = values[2] + value;
+            values[2] = newAim;
         }
     }
 
